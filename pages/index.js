6a2f287ba24f4624db4f6bc8915d56e84ad1ec88
MeetRajput00 <rajputmeet10@gmail.com>
2022-08-21 01:21:23 +0530

    doctor ui done

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,12 @@ export default function Home() {
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            background: "#aee571"
+            background: "#76b852", /* fallback for old browsers */
+            background: "rgb(141,194,111)",
+            background: "linear-gradient(90deg, rgba(141,194,111,1) 0%, rgba(118,184,82,1) 50%)"
           }}>
             <div className={styles.logo}>
               MediDoc
-              <Image src={icon} width={100} height={100} />
             </div>
             <button className={styles.signout} onClick={logout}>Sign Out</button>
           </div>
